b57cca7d80feb2665d0a811081c2c576d55c2e4c
Axlfc <axelfernandezcurros@gmail.com>
2022-09-21 18:01:42 +0200

    UPDATED: Exercise 4.1

--- 4.-Conditional_structures_exercises/1.cpp
+++ 4.-Conditional_structures_exercises/1.cpp
@@ -7,34 +7,25 @@ using namespace std;
  /*1.   Realizar un programa que lea un valor entero y determine si se trata de un número par
 o impar*/
 
-
-float notaFinal(float total, float n){
-    return total / n;
+bool isPair(int n) {
+    if (n % 2 == 0) {
+        return true; 
+    }
+    else {
+        return false;
+    }
+    
 }
 
 int main() {
-    bool final = false;
-    float total = 0;
-    string x;
-    float y = 0;
-    int c = -1;
-    while ( !final ) {
-        cout << "\n(Type 'FIN' to display result)\nType qualifiaction:\n\n"; // Type a number and press enter
-        cin >> x;
-        if ( x == "FIN" ) {
-            final = true;
-        }
-
-        // Convert string to float
-        y = atof(x.c_str());
-        if (y <= 10) {
-            total += y;
-            c++;
-        }
-        else {
-            cout << "Enter qualifiaction value lower than 10!\n";
-            return 1;
-        }
+    int x;
+    cout << "\nType number:\n\n"; // Type a number and press enter
+    cin >> x;
+    if (isPair(x)) {
+        cout << "\nNumber " << x << " is pair.\n";
     }
-    cout << "\nNota final:\t" << notaFinal(total, c) << "\n";
+    else {
+        cout << "\nNumber " << x << " is odd.\n";
+    }
+    
 }
