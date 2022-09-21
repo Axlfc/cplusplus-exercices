4397a3dd81ca57f209380a2e4dd644eb0e35070b
Axlfc <axelfernandezcurros@gmail.com>
2022-09-21 18:44:35 +0200

    UPDATED: Adding templates for the exercises

---
+++ 16.-Object_oriented_exercises/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+ /*4.   Realizar un programa en el cual se construya una solución para la jerarquía (herencia)
+de clases mostrada en la siguiente figura:*/
+    //      -->Trabajador
+// Persona --
+    //      -->Estudiante-->Estudiante de 2º año
+int main() {
+    
+    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
+    cin >> x;
+       
+}
