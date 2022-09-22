6999718c775eb54e05ed29a7b58ace0235986530
1vrv <1vrv@TartangaLH.eus>
2022-09-22 21:22:51 +0200

    UPDATED: Adding exercise 6.1

--- 6.-Vectors_exercises/1.cpp
+++ 6.-Vectors_exercises/1.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-
+#include <vector>
 using namespace std;
 
  /*1.   Escribir un programa que defina un vector de números y calcule: la suma y la
 multiplicación de sus elementos.*/
 
+int calculeSum(vector<int> vect) {
+    int result = 0;
+    for (int x : vect) {
+        result += x;
+    }
+    return result;
+}
+
+
+int calculeMult(vector<int> vect) {
+    int result = 0;
+    for (int x : vect) {
+        result *= x;
+    }
+    return result;
+}
 
 
 int main() {
-    
-    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
-    cin >> x;
-       
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+  
+    vector<int> vect(arr, arr + n);
+  
+    cout << "The elements of the vector summed are:\t" << calculeSum(vect) << "\n";
+    cout << "The elements of the vector multiplied are:\t" << calculeMult(vect) << "\n";
+    return 0;
 }
