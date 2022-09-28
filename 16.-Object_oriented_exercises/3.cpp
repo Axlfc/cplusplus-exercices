850c5bff4550e8309f7bf727f016061496d93876
Axlfc <axelfernandezcurros@gmail.com>
2022-09-28 17:18:09 +0200

    ADDED: Exercise 16.3

--- 16.-Object_oriented_exercises/3.cpp
+++ 16.-Object_oriented_exercises/3.cpp
@@ -7,11 +7,73 @@ using namespace std;
  /*3.   Crear un programa que tenga la siguiente jerarquía de clases: animal(clase padre) →
 Humano (Clase hija) → Perro (clase Hija), y hacer polimorfismo con el método comer().*/
 
+class Animal {
+    private:
+        int age;
+    public:
+        Animal(int);  // Constructor
+        virtual void eat();  // Polymorphism 
+};
+
+
+class Human:    public Animal {
+    private:
+        string name;
+    public:
+        Human(int, string);
+        void eat();  // Inherited polyphormism
+};
+
+
+class Dog:  public Animal {
+    private:
+        string name, race;
+    public:
+        Dog(int, string, string);
+        void eat();  // Inherited polyphormism
+};
+
+
+Animal::Animal(int _age) {  // Constructor
+    age = _age;
+}
+
+void Animal::eat() {
+    cout << "I eat...";
+}
+
+
+Human::Human(int _age, string _name): Animal(_age) {
+    name = _name;
+}
+
+void Human::eat() {
+    Animal::eat();
+    cout << "at the table.\n";
+}
+
+
+Dog::Dog(int _age, string _name, string _race): Animal(_age) {
+    name = _name;
+    race = _race;
+}
+
+void Dog::eat() {
+    Animal::eat();
+    cout << "at the floor.\n";
+}
 
 
 int main() {
-    
-    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
-    cin >> x;
+    Animal *animals[100];
+
+    animals[0] = new Human(26, "Axel");
+
+    animals[1] = new Dog(4, "Snoop", "Doggity Dog");
+
+    animals[0] -> eat();
+
+    animals[1] -> eat();
+
        
 }
