5f08718631fd07c1c4b2985c050d67fae1a4e8bc
Axlfc <axelfernandezcurros@gmail.com>
2022-09-29 09:17:23 +0200

    ADDED: Ex. 06.3

--- 06.-Vectors_exercises/3.cpp
+++ 06.-Vectors_exercises/3.cpp
@@ -7,11 +7,29 @@ using namespace std;
  /*3.   Hacer un programa que lea 5 números, los copie en otro vector multiplicado por 2 y se
 muestre por pantalla.*/
 
-
+int calculeMult(vector<int> vect) {
+    int result = 0;
+    for (int x : vect) {
+        result *= x;
+    }
+    return result;
+}
 
 int main() {
+    int s = 5;
+    int a;
+    vector<int> vect;
+
+    for(int i = 0; i < s; i++) {
+        cin>>a;
+        vect.push_back(a);
+    }
+    cout << "\n";
     
-    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
-    cin >> x;
+    for(auto &p: vect) {
+      cout << p * 2 << " ";
+    }
+   cout << "\n";
+   return 0;
        
 }
