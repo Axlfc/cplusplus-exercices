a8b786f0a198045e2924da210616d27f047683fe
Axlfc <axelfernandezcurros@gmail.com>
2022-10-23 17:44:06 +0200

    ADDED: Exercises 13.1 & 13.2

--- 13.-Queues_exercises/1.cpp
+++ 13.-Queues_exercises/1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -9,8 +10,27 @@ using namespace std;
 
 
 int main() {
-    
-    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
-    cin >> x;
+    queue<int> newqueue;
+    bool isUserDone = false;
+    string x = "0";
+    int y = 0;
+    while (!isUserDone) {
+        cout << "\n(Type a number to add to queue:\n\n"; // Type a number and press enter
+        cin >> x;
+        try {
+            y = stoi(x);
+            newqueue.push(y);
+        }
+        catch (invalid_argument &e) {
+            isUserDone = true;
+        }
+    };
+    // Print stack result
+    while (!newqueue.empty()) {
+        cout << newqueue.front() << ' ';
+        newqueue.pop();
+    }
+    cout << "\n";
+    return 0;
        
 }
--- 13.-Queues_exercises/2.cpp
+++ 13.-Queues_exercises/2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include<curses.h>
+#include<stdlib.h>
 
 using namespace std;
 
@@ -8,10 +10,107 @@ using namespace std;
 último muestre los clientes en el orden correcto.*/
 
 
+void clean_stdin(void)
+{
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+
+struct Client{
+	char name[30];
+	char bill[10];
+	int age;
+};
+
+
+struct Node{
+	Client c;
+	Node *next;
+};
+
+
+// Functions prototypes
+void loadClient(Client &);
+void insertQueue(Node *&, Node *&, Client);
+bool emptyQueue(Node *);
+void removeQueue(Node *&, Node *&, Client &);
+
 
 int main() {
-    
-    cout << "\n(Type noseke:\n\n"; // Type a number and press enter
-    cin >> x;
-       
+    Node *front = NULL;
+	Node *end = NULL;
+	Client c;
+	char ans;
+
+	do{
+		loadClient(c);
+		insertQueue(front,end,c);
+
+		cout<<"Do you want to add more clients? (y/n): ";
+		cin >> ans;
+		cout << "\n";
+	} while(ans == 'Y' || ans == 'y');
+
+	cout << "\n\n=Client Load Successful=\n\n";
+	cout<<"Showing clients:\n\n";
+
+	while(front != NULL) {
+		removeQueue(front, end, c);
+		cout<<"Name: " <<c .name << "\n";
+		cout<<"Bill: " << c.bill << "\n";
+		cout<<"Age: " << c.age << "\n";
+		cout << "\n";
+	}
+	return 0;
+}
+
+
+void loadClient(Client &c){
+	clean_stdin();
+	cout << "\tAdding new client" << "\n";
+	cout << "Name: ";
+	cin.getline(c.name, 30, '\n');
+	cout << "Bill: ";
+	cin.getline(c.bill, 10, '\n');
+	cout << "Age: ";
+	cin >> c.age;
+	cout << "\n";
+}
+
+void insertQueue(Node *&front, Node *&end, Client c){
+	Node *newNode = new Node();
+
+	newNode -> c = c;
+	newNode -> next = NULL;
+
+	if(emptyQueue(front)){
+		front = newNode;
+	}
+	else{
+		end -> next = newNode;
+	}
+
+	end = newNode;
+}
+
+bool emptyQueue(Node *front){
+	return (front == NULL)? true : false;
+}
+
+void removeQueue(Node *&front, Node *&end, Client &c){
+	c = front -> c;
+	Node *aux = front;
+
+	if(front == end){
+		front = NULL;
+		end = NULL;
+	}
+	else{
+		front = front -> next;
+	}
+
+	delete aux;
 }
